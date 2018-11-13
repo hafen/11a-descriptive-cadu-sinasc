d94ded1232899602787fe69598b7f1308bda9354
hafen <rhafen@gmail.com>
2018-11-13 17:35:33 -0300

    Updates

--- data/scripts/sinasc_process.R
+++ data/scripts/sinasc_process.R
@@ -175,8 +175,128 @@ hist(snsc2$brthwt_z)
 #   ) %>%
 #   write_csv("~/Desktop/snsc_dictionary.csv")
 
-kitools::data_publish(snsc2, name = "snsc_2011-2015",
-  desc = "Public individual-level SINASC data from 2011-2015, with variables that intersect with the CIDACS GCE data dictionary.")
+
+# make Ignored, Null, etc. NA
+isfct <- which(unlist(lapply(d, is.factor)))
+lvls <- sort(unique(unlist(lapply(d[isfct], levels))))
+ign <- c("Ignored", "Inconsistency", "Null",
+  "Not applicable")
+for (nm in names(isfct)) {
+  idx <- which(d[[nm]] %in% ign)
+  message(nm, " ", length(idx))
+  d[[nm]][idx] <- NA
+  d[[nm]] <- droplevels(d[[nm]])
+}
+
+# only look at single births
+d <- filter(d, preg_type == "Singleton")
+
+# add birth weight centile
+d$brthwt_centile <- NA
+idx <- which(!is.na(d$sex))
+
+d$brthwt_centile[idx] <- igb_wtkg2centile(d$gest_weeks[idx] * 7, d$brthwt_g[idx] / 1000, sex = as.character(d$sex[idx]))
+
+hist(d$brthwt_centile)
+
+## Preterm 
+d <- d %>% 
+  mutate(preterm = if_else(gest_weeks < 28, true = 1, false = 
+      if_else(gest_weeks >= 28 & gest_weeks < 32, true = 2, false = 
+          if_else(gest_weeks >= 32 & gest_weeks < 37, true = 3, false = 
+            if_else(gest_weeks == 37 | gest_weeks == 38,
+              true = 4, false = 5)))))
+
+## Preterm Intervention
+d <- d %>% 
+  mutate(preterm_interv = if_else(gest_weeks < 37 & labor_induced == "No", true = 1, false = 
+      if_else(gest_weeks < 37 & labor_induced == "Yes", true = 2, false = 
+          if_else(gest_weeks >= 37, true = 3, false = 4))))
+
+## Adding Labels
+d$preterm <- recode(d$preterm, "1" = "Extremely Preterm", "2" = "Very Preterm", "3" = "Moderate or Late Premature", "4" = "Early Term", "5" = "Normal")
+d$preterm_interv <- recode(d$preterm_interv, "1" = "Not induced Preterm", "2" = "Induced Preterm", "3" = "Not Preterm")
+
+###  Percentile  ###   AIG = 0 (10 =< Percentile <= 90)   GIG = 1 (Percentile > 90)     PIG = 2 (Percentile < 10)
+d$brthwt_centile_cat[d$brthwt_centile >= 10 & d$brthwt_centile <= 90] <- 0   
+d$brthwt_centile_cat[d$brthwt_centile > 90] <- 1 
+d$brthwt_centile_cat[d$brthwt_centile < 10] <- 2
+
+d$brthwt_centile_cat <- recode(d$brthwt_centile_cat,
+  "0" = "10 =< Percentile <= 90 (Normal)",
+  "1" = "Percentile > 90 (LGA)",
+  "2" = "Percentile < 10 (SGA)"
+)
+
+d$gest_weeks2 <- as.numeric(difftime(d$birth_date, 
+  d$menstrual_date_last, units = "weeks"))
+
+length(which(d$gest_weeks2 > 50))
+
+d$gest_weeks2[d$gest_weeks2 > 50] <- NA
+d$gest_weeks2[d$gest_weeks2 < 20] <- NA
+
+hist(d$gest_weeks2, breaks = 30)
+
+d$brthwt_z2 <- NA
+idx <- which(!is.na(d$sex))
+library(growthstandards)
+d$brthwt_z2[idx] <- igb_wtkg2zscore(d$gest_weeks2[idx] * 7, d$brthwt_g[idx] / 1000, sex = as.character(d$sex[idx]))
+
+d$brthwt_z2[is.infinite(d$brthwt_z2)] <- NA
+d$brthwt_z2[d$brthwt_z2 < -8] <- NA
+d$brthwt_z2[d$brthwt_z2 > 8] <- NA
+
+hist(d$brthwt_z2)
+
+# add birth weight centile
+d$brthwt_centile2 <- NA
+idx <- which(!is.na(d$sex))
+
+d$brthwt_centile2[idx] <- igb_wtkg2centile(d$gest_weeks2[idx] * 7, d$brthwt_g[idx] / 1000, sex = as.character(d$sex[idx]))
+
+hist(d$brthwt_centile2)
+
+## Preterm 
+d <- d %>% 
+  mutate(preterm2 = if_else(gest_weeks2 < 28, true = 1, false = 
+      if_else(gest_weeks2 >= 28 & gest_weeks2 < 32, true = 2, false = 
+          if_else(gest_weeks2 >= 32 & gest_weeks2 < 37, true = 3, false = 
+            if_else(gest_weeks2 == 37 | gest_weeks2 == 38,
+              true = 4, false = 5)))))
+
+## Preterm Intervention
+d <- d %>% 
+  mutate(preterm_interv2 = if_else(gest_weeks2 < 37 & labor_induced == "No", true = 1, false = 
+      if_else(gest_weeks2 < 37 & labor_induced == "Yes", true = 2, false = 
+          if_else(gest_weeks2 >= 37, true = 3, false = 4))))
+
+## Adding Labels
+d$preterm2 <- recode(d$preterm2, "1" = "Extremely Preterm", "2" = "Very Preterm", "3" = "Moderate or Late Premature", "4" = "Early Term", "5" = "Normal")
+d$preterm_interv2 <- recode(d$preterm_interv2, "1" = "Not induced Preterm", "2" = "Induced Preterm", "3" = "Not Preterm")
+
+###  Percentile  ###   AIG = 0 (10 =< Percentile <= 90)   GIG = 1 (Percentile > 90)     PIG = 2 (Percentile < 10)
+d$brthwt_centile_cat2[d$brthwt_centile2 >= 10 & d$brthwt_centile2 <= 90] <- 0   
+d$brthwt_centile_cat2[d$brthwt_centile2 > 90] <- 1 
+d$brthwt_centile_cat2[d$brthwt_centile2 < 10] <- 2
+
+d$brthwt_centile_cat2 <- recode(d$brthwt_centile_cat2,
+  "0" = "10 =< Percentile <= 90 (Normal)",
+  "1" = "Percentile > 90 (LGA)",
+  "2" = "Percentile < 10 (SGA)"
+)
+
+kitools::data_publish(d, name = "snsc_2011-2015",
+  desc = "Public individual-level SINASC data from 2011-2015, with variables that intersect with the CIDACS GCE data dictionary and additional derived variables.")
+
+
+
+
+
+
+
+
+
 
 
 ignore <- c(
@@ -203,77 +323,3 @@ ignore <- c(
 )
 
 data.table::fwrite(snsc2[, setdiff(names(snsc2), ignore)], "~/Desktop/snsc_ml.csv")
-
-
-
-
-
-
-
-
-
-
-
-
-d$birth_date <- as.Date(substr(d$birth_date, 1, 10))
-d$menstrual_date_last <- as.Date(d$menstrual_date_last)
-
-d$gest_weeks2 <- as.numeric(difftime(d$birth_date, d$menstrual_date_last, units = "weeks"))
-
-length(which(d$gest_weeks2 > 50))
-
-d$gest_weeks2[d$gest_weeks2 > 50] <- NA
-d$gest_weeks2[d$gest_weeks2 < 20] <- NA
-
-hist(d$gest_weeks2, breaks = 30)
-
-
-d$brthwt_z2 <- NA
-idx <- which(!is.na(d$sex))
-
-library(growthstandards)
-d$brthwt_z2[idx] <- igb_wtkg2zscore(d$gest_weeks2[idx] * 7, d$brthwt_g[idx] / 1000, sex = as.character(d$sex[idx]))
-
-hist(d$brthwt_z2)
-# looks pretty good
-
-# add birth weight centile
-d$brthwt_centile2 <- NA
-idx <- which(!is.na(d$sex))
-
-d$brthwt_centile2[idx] <- igb_wtkg2centile(d$gest_weeks2[idx] * 7, d$brthwt_g[idx] / 1000, sex = as.character(d$sex[idx]))
-
-hist(d$brthwt_centile2)
-
-
-## Preterm 
-d <- d %>% 
-  mutate(preterm2 = if_else(gest_weeks2 < 28, true = 1, false = 
-      if_else(gest_weeks2 >= 28 & gest_weeks2 < 32, true = 2, false = 
-          if_else(gest_weeks2 >= 32 & gest_weeks2 < 37, true = 3, false = 
-              if_else(gest_weeks2 == 37 | gest_weeks2 == 38, true = 4, false = 5)))))
-
-## Preterm Intervention
-
-d <- d %>% 
-  mutate(preterm_interv2 = if_else(gest_weeks2 < 37 & labor_induced == "No", true = 1, false = 
-      if_else(gest_weeks2 < 37 & labor_induced == "Yes", true = 2, false = 
-          if_else(gest_weeks2 >= 37, true = 3, false = 4))))
-
-## Adding Labels
-
-d$preterm2 <- recode(d$preterm2, "1" = "Extremely Preterm", "2" = "Very Preterm", "3" = "Moderate or Late Premature", "4" = "Early Term", "5" = "Normal")
-d$preterm_interv2 <- recode(d$preterm_interv2, "1" = "Not induced Preterm", "2" = "Induced Preterm", "3" = "Not Preterm")
-
-###  Percentile  ###   AIG = 0 (10 =< Percentile <= 90)   GIG = 1 (Percentile > 90)     PIG = 2 (Percentile < 10)
-d$brthwt_centile_cat2[d$brthwt_centile2 >= 10 & d$brthwt_centile2 <= 90] <- 0   
-d$brthwt_centile_cat2[d$brthwt_centile2 > 90] <- 1 
-d$brthwt_centile_cat2[d$brthwt_centile2 < 10] <- 2
-
-d$brthwt_centile_cat2 <- recode(d$brthwt_centile_cat2,
-  "0" = "10 =< Percentile <= 90 (Normal)",
-  "1" = "Percentile > 90 (LGA)",
-  "2" = "Percentile < 10 (SGA)"
-)
-
-
